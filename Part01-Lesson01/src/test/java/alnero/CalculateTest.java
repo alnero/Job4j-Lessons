106475316e359a48a43c567aee19fc82ac54a592
Nerodenko Alexey <darestream@gmail.com>
2016-12-03 16:57:53 +0300

    Add test for Part01-Lesson01 - Hello World program.

---
+++ Part01-Lesson01/src/test/java/alnero/CalculateTest.java
@@ -0,0 +1,16 @@
+package alnero;
+
+import org.junit.Test;
+
+/**
+ * Testing Hello World.
+ */
+public class CalculateTest {
+    /**
+     * Testing of Hello World output.
+     */
+    @Test
+    public void whenRunMainMethodThenHelloWorldPrintedToConsole() {
+        Calculate.main(null);
+    }
+}
