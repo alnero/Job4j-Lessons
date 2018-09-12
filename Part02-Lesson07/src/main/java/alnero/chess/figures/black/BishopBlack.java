c1d754318828f46f95f2d194cf225827b9895d8b
alnero <darestream@gmail.com>
2018-09-12 20:23:00 +0300

    Add solution for Part02-Lesson07, black bishop move

--- Part02-Lesson07/src/main/java/alnero/chess/figures/black/BishopBlack.java
+++ Part02-Lesson07/src/main/java/alnero/chess/figures/black/BishopBlack.java
@@ -1,5 +1,6 @@
 package alnero.chess.figures.black;
 
+import alnero.chess.ImpossibleMoveException;
 import alnero.chess.figures.Cell;
 import alnero.chess.figures.Figure;
 
@@ -25,8 +26,49 @@ public class BishopBlack implements Figure {
     }
 
     @Override
-    public Cell[] way(Cell source, Cell dest) {
-        return new Cell[] {dest};
+    public Cell[] way(Cell source, Cell dest) throws ImpossibleMoveException {
+        Cell[] steps = new Cell[0];
+
+        if (Math.abs(source.x - dest.x) == Math.abs(source.y - dest.y)) {
+            int numberOfSteps = Math.abs(source.x - dest.x);
+            steps = new Cell[numberOfSteps];
+
+            for (int i = 0; i < numberOfSteps; i++) {
+                // move North-East
+                if (source.x < dest.x && source.y > dest.y) {
+                    for (Cell cell : Cell.values()) {
+                        if (cell.x == source.x + 1 + i && cell.y == source.y - 1 - i) {
+                            steps[i] = cell;
+                        }
+                    }
+                // move South-East
+                } else if (source.x < dest.x && source.y < dest.y) {
+                    for (Cell cell : Cell.values()) {
+                        if (cell.x == source.x + 1 + i && cell.y == source.y + 1 + i) {
+                            steps[i] = cell;
+                        }
+                    }
+                // move South-West
+                } else if (source.x > dest.x && source.y < dest.y) {
+                    for (Cell cell : Cell.values()) {
+                        if (cell.x == source.x - 1 - i && cell.y == source.y + 1 + i) {
+                            steps[i] = cell;
+                        }
+                    }
+                // move North-West
+                } else {
+                    for (Cell cell : Cell.values()) {
+                        if (cell.x == source.x - 1 - i && cell.y == source.y - 1 - i) {
+                            steps[i] = cell;
+                        }
+                    }
+                }
+            }
+        } else {
+            throw new ImpossibleMoveException(this.getClass().getName() + " impossible move.");
+        }
+
+        return steps;
     }
 
     @Override
