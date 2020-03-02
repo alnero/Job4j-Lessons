f2ad5f022a5c96e98ed6de903edecdec87e5e4e9
alnero <darestream@gmail.com>
2020-03-02 16:57:08 +0300

    Add solution for Part05-Lesson05, create user model

---
+++ Part05-Lesson05/src/main/java/alnero/simpleMap/User.java
@@ -0,0 +1,27 @@
+package alnero.simpleMap;
+
+import java.util.Calendar;
+
+/**
+ * User class for Map storage.
+ */
+public class User {
+    /** User name. **/
+    private String name;
+    /** Number of children. **/
+    private int children;
+    /** User birth date. **/
+    private Calendar birthday;
+
+    /**
+     * Simple user constructor.
+     * @param name user name
+     * @param children number of children
+     * @param birthday birth date
+     */
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
