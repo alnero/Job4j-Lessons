8b9e34b4f83d22a59f9e1ae75d68e092a9e5b83a
alnero <darestream@gmail.com>
2020-04-26 13:02:51 +0300

    Add solution for Part05-Lesson06, simple tree, check if tree is binary

--- Part05-Lesson06/src/main/java/alnero/simpleTree/Tree.java
+++ Part05-Lesson06/src/main/java/alnero/simpleTree/Tree.java
@@ -1,5 +1,6 @@
 package alnero.simpleTree;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.LinkedList;
@@ -10,6 +11,26 @@ class Tree<E> implements SimpleTree<E> {
 
     Tree(final E root) {
         this.root = new Node<>(root);
+    }
+
+    /**
+     * Check that nodes do not have more than two child nodes.
+     * @return is tree binary or not
+     */
+    public boolean isBinary() {
+        boolean result = true;
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            List<Node<E>> children = el.getChildren();
+            if (children.size() > 2) {
+                result = false;
+                break;
+            }
+            data.addAll(children);
+        }
+        return result;
     }
 
     @Override
--- Part05-Lesson06/src/test/java/alnero/simpleTree/TreeTest.java
+++ Part05-Lesson06/src/test/java/alnero/simpleTree/TreeTest.java
@@ -31,4 +31,35 @@ public class TreeTest {
         assertThat(tree.findBy(7).isPresent(), is(false)
         );
     }
+
+    @Test
+    public void whenBinaryTreeThenReturnTrue() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 4);
+        tree.add(2, 5);
+        tree.add(4, 6);
+        tree.add(5, 7);
+        assertThat(tree.isBinary(), is(true));
+    }
+
+    @Test
+    public void whenNotBinaryTreeThenReturnFalse() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 4);
+        tree.add(2, 5);
+        tree.add(2, 6);
+        tree.add(4, 6);
+        tree.add(4, 7);
+        assertThat(tree.isBinary(), is(false));
+    }
+
+    @Test
+    public void whenOnlyRootNodeAddedThenIsBinaryReturnTrue() {
+        Tree<Integer> tree = new Tree<>(1);
+        assertThat(tree.isBinary(), is(true));
+    }
 }
