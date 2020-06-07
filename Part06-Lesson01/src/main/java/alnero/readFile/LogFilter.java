162fabb3f3e79315bdd36f5782e46347e075c430
alnero <darestream@gmail.com>
2020-06-07 15:33:17 +0300

    Add solution for Part06-Lesson01, save 404 strings to file

--- Part06-Lesson01/src/main/java/alnero/readFile/LogFilter.java
+++ Part06-Lesson01/src/main/java/alnero/readFile/LogFilter.java
@@ -1,17 +1,20 @@
 package alnero.readFile;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
- * Filter log file.
+ * Filter log file and save results.
  */
 public class LogFilter {
     /**
      * Filter log file and get strings with return code 404.
-     * @param file log file
+     * @param file log file name
      * @return list of strings with return code 404
      */
     public List<String> filter(String file) {
@@ -25,5 +28,18 @@ public class LogFilter {
             e.printStackTrace();
         }
         return result;
+    }
+
+    /**
+     * Save list of strings to a file.
+     * @param log list of strings
+     * @param file destination file name
+     */
+    public void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            log.forEach(str -> out.write(str + System.lineSeparator()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- Part06-Lesson01/src/test/java/alnero/readFile/LogFilterTest.java
+++ Part06-Lesson01/src/test/java/alnero/readFile/LogFilterTest.java
@@ -1,17 +1,30 @@
 package alnero.readFile;
 
-import java.util.List;
-import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 /**
- * Test reading of strings from log file.
+ * Test reading and writing strings from log file.
  */
 public class LogFilterTest {
+    /**
+     * Temp folder for test files.
+     */
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void whenReadStringsFromLogFileThenOnlyStringsWith404Returned() {
         // redirect console output for test
@@ -28,9 +41,32 @@ public class LogFilterTest {
         assertThat(byteArrayOutputStream.toString(), is(
         "0:0:0:0:0:0:0:1 - - [19/Feb/2020:15:21:18 +0300] \"GET /items/ajax.html HTTP/1.1\" 404 1113" + System.lineSeparator()
             + "0:0:0:0:0:0:0:1 - - [19/Feb/2020:15:21:23 +0300] \"GET /TrackStudio/ HTTP/1.1\" 404 1110"  + System.lineSeparator()
-            + "0:0:0:0:0:0:0:1 - - [19/Feb/2020:15:21:34 +0300] \"GET /TrackStudioNew/ HTTP/1.1\" 404 -"  + System.lineSeparator()));
+            + "0:0:0:0:0:0:0:1 - - [19/Feb/2020:15:21:34 +0300] \"GET /TrackStudioNew/ HTTP/1.1\" 404 -"  + System.lineSeparator()
+        ));
         // restore console output
         System.out.flush();
         System.setOut(systemOut);
+    }
+
+    @Test
+    public void whenWrite404StringsToFileThenProperFileWritten() throws IOException {
+        LogFilter logReader = new LogFilter();
+        List<String> log = logReader.filter("src/test/resources/log.txt");
+        File file = folder.newFile("404.txt");
+        logReader.save(log, file.getAbsolutePath());
+
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            String line = in.readLine();
+            while (line != null) {
+                result.append(line).append(System.lineSeparator());
+                line = in.readLine();
+            }
+        }
+        assertThat(result.toString(), is(
+        "0:0:0:0:0:0:0:1 - - [19/Feb/2020:15:21:18 +0300] \"GET /items/ajax.html HTTP/1.1\" 404 1113" + System.lineSeparator()
+            + "0:0:0:0:0:0:0:1 - - [19/Feb/2020:15:21:23 +0300] \"GET /TrackStudio/ HTTP/1.1\" 404 1110"  + System.lineSeparator()
+            + "0:0:0:0:0:0:0:1 - - [19/Feb/2020:15:21:34 +0300] \"GET /TrackStudioNew/ HTTP/1.1\" 404 -"  + System.lineSeparator()
+        ));
     }
 }
