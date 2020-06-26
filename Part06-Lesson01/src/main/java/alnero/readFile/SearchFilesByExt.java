7d4b5e1e5dda7d9e238c18230ad88fe5d63e8aa2
alnero <darestream@gmail.com>
2020-06-26 16:50:25 +0300

    Add solution for Part06-Lesson01, search files by extension using cl args

--- Part06-Lesson01/src/main/java/alnero/readFile/SearchFilesByExt.java
+++ Part06-Lesson01/src/main/java/alnero/readFile/SearchFilesByExt.java
@@ -1,12 +1,13 @@
 package alnero.readFile;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.nio.file.FileVisitor;
 import java.nio.file.FileVisitResult;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.IOException;
 import java.nio.file.attribute.BasicFileAttributes;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
@@ -43,5 +44,20 @@ public class SearchFilesByExt {
         };
         Files.walkFileTree(root, fileVisitor);
         return result;
+    }
+
+    /**
+     * Search files from command line, two arguments required: path to search folder, file extension.
+     * @param args path to search folder and file extension
+     * @throws IOException exceptions from FileVisitor API
+     */
+    public static void main(String[] args) throws IOException {
+        if (args.length <= 1) {
+            throw new IllegalArgumentException("Arguments required, usage: java -cp Part06-Lesson01-1.0.0.jar alnero.readFile.SearchFilesByExt PATH_TO_SEARCH_FOLDER FILE_EXTENSION");
+        }
+        SearchFilesByExt searchFiles = new SearchFilesByExt();
+        Path path = Paths.get(args[0]);
+        List<Path> pathsToFiles = searchFiles.search(path, args[1]);
+        pathsToFiles.forEach(System.out::println);
     }
 }
--- Part06-Lesson01/src/test/java/alnero/readFile/SearchFilesByExtTest.java
+++ Part06-Lesson01/src/test/java/alnero/readFile/SearchFilesByExtTest.java
@@ -1,5 +1,7 @@
 package alnero.readFile;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -23,5 +25,45 @@ public class SearchFilesByExtTest {
         List<String> result = pathsToTxtFiles.stream().map(Path::getFileName).map(Path::toString).collect(Collectors.toList());
         List<String> expected = Arrays.asList("numbers.txt", "log.txt");
         assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenNoArgumentsThenThrowAndMessageHowToUseProgramFromCommandLine() {
+        String result = "";
+        try {
+            SearchFilesByExt.main(new String[]{});
+        } catch (Exception e) {
+            result = e.getMessage();
+        }
+        assertThat(result, is("Arguments required, usage: java -cp Part06-Lesson01-1.0.0.jar alnero.readFile.SearchFilesByExt PATH_TO_SEARCH_FOLDER FILE_EXTENSION"));
+    }
+
+    @Test
+    public void whenOnlyOneArgumentThenThrowAndMessageHowToUseProgramFromCommandLine() {
+        String result = "";
+        try {
+            SearchFilesByExt.main(new String[]{"src/test/resources"});
+        } catch (Exception e) {
+            result = e.getMessage();
+        }
+        assertThat(result, is("Arguments required, usage: java -cp Part06-Lesson01-1.0.0.jar alnero.readFile.SearchFilesByExt PATH_TO_SEARCH_FOLDER FILE_EXTENSION"));
+    }
+
+    @Test
+    public void whenTwoCorrectArgumentThenProperResultsReturned() throws IOException {
+        // redirect console output for test
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream mockOut = new PrintStream(byteArrayOutputStream);
+        PrintStream systemOut = System.out;
+        System.setOut(mockOut);
+        // search txt files
+        SearchFilesByExt.main(new String[]{"src/test/resources", "txt"});
+        assertThat(byteArrayOutputStream.toString(), is(
+                "src/test/resources/numbers.txt"  + System.lineSeparator()
+                    + "src/test/resources/log.txt" + System.lineSeparator()
+        ));
+        // restore console output
+        System.out.flush();
+        System.setOut(systemOut);
     }
 }
